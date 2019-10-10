ffeb4cd22efbe2b9977fb44e894f5728133def32
Victor <you@example.com>
2019-10-10 14:50:05 -0300

    refactoring

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.get('/', async function (req, res, next) {
   res.send('respond with a resource');
 });
 
-String.prototype.replaceAll = function(valueToReplace, newValue){
+String.prototype.replaceAll = function (valueToReplace, newValue) {
   let newString = this;
   while (newString.indexOf(valueToReplace) >= 0) {
     newString = newString.replace(valueToReplace, newValue)
@@ -22,20 +22,10 @@ async function loadRss() {
   request('https://trends.google.com.br/trends/trendingsearches/daily/rss?geo=BR', { json: true }, async (err, res, body) => {
     if (err) { return console.log(err); }
 
-    body = body.replaceAll("ht:", "");
-    body = body.replaceAll("+", "");
+    let items = await parseXmlToItems(body);
+    await saveItems(body);
 
-    let parserOptions = { customFields: { item: ['approx_traffic','news_item'] } };
-    let parser = new Parser(parserOptions);
-
-    let feed = await parser.parseString(body);
-  
-    let items = feed.items;
-    items = items.sort((a,b)=>{
-      return Number(b.approx_traffic.replace(",",".")) - Number(a.approx_traffic.replace(",","."));
-    });
-
-    feed.items.forEach(item => {
+    items.forEach(item => {
       console.log("--------------------------------------------------------------");
       console.log(`${item.title} (${item.approx_traffic})`);
       console.log(`Data: ${item.isoDate}`);
@@ -43,6 +33,39 @@ async function loadRss() {
       console.log(`Data: ${item.pubDate}`);
     });
   });
+}
+
+async function saveItems(items){
+
+}
+
+async function parseXmlToItems(body) {
+  body = body.replaceAll("ht:", "");
+  body = body.replaceAll("+", "");
+
+  let parserOptions = { customFields: { item: ['approx_traffic', 'news_item'] } };
+  let parser = new Parser(parserOptions);
+
+  let feed = await parser.parseString(body);
+
+  let items = feed.items;
+
+  items = formatValues(items);
+
+  items = items.sort((a, b) => {
+    return b.approx_traffic - a.approx_traffic;
+  });
+
+  return items;
+}
+
+function formatValues(items) {
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    item.isoDate = new Date(item.isoDate);
+    item.approx_traffic = Number(item.approx_traffic.replaceAll(",", ""));
+  }
+  return items;
 }
 
 const https = require('https');
